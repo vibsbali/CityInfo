12a88704122c6f5dba563caa254d9bf7cfc81143
Vaibhav Bali <vbali@internode.on.net>
2023-07-06 07:05:52 +1000

    Add project files.

---
+++ CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,26 @@
+﻿using CityInfo.API;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            var result = CitiesDataStore.Current.Cities;
+
+            return new JsonResult(result);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult GetCity(int id)
+        {
+            var result = CitiesDataStore.Current.Cities.Where(c => c.Id == id);
+
+            return new JsonResult(result);
+        }
+    }
+}
